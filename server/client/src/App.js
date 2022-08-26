b81c350a4d49398b3921fb431c25e9312c503349
Sigrsig <sospsig@gmail.com>
2022-08-26 14:15:50 +0200

    prep for heroku

---
+++ server/client/src/App.js
@@ -0,0 +1,12 @@
+import Generator from "./components/Generator";
+import styles from "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Generator />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/client/src/components/getStatement.js
@@ -0,0 +1,11 @@
+const getStatement = () => {
+
+const statements = [
+    `Statement of`
+] 
+
+};
+
+export default getStatement;
+
+
