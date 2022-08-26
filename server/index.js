3262c5bd17e0aef116aaf46bee63c83e1ca12ef8
Sigrsig <sospsig@gmail.com>
2022-08-26 14:27:58 +0200

    More Heroku stuff

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const helmet = require("helmet"); // creates headers that protect from attacks (security)
+const cors = require("cors"); // allows/disallows cross-site communication
+
 const firstnames = require("./routes/firstnames");
 const lastnames = require("./routes/lastnames");
 const nouns = require("./routes/nouns");
@@ -7,6 +10,28 @@ const adj = require("./routes/adj");
 const jobs = require("./routes/jobs");
 
 const app = express();
+
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:1234",
+  "https://shrouded-journey-38552.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(helmet());
+// --> Add this
+app.use(cors(corsOptions));
 
 app.use("/api/firstnames", firstnames);
 app.use("/api/lastnames", lastnames);
@@ -18,5 +43,14 @@ app.use("/api/jobs", jobs);
 app.get("/api", (req, res) => {
   res.send("Hello there from express");
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.listen(1234);
